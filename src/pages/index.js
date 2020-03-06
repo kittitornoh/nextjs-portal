87ce81db951ea7b929e1cbf41934a67abe0ca36a
kithodges <khodges@inxitehealth.com>
2020-03-06 11:33:07 -0500

    set up boilerplate log in

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,7 @@
-const Home = () => {
-  return (
-    <div id='root'>
-      <h3>Inxite Health</h3>
-      <style global jsx>{`
-        /* full height page */
-        html,
-        body,
-        body > div:first-child,
-        div#__next,
-        div#__next > div,
-        div#__next > div > div {
-          height: 100%;
-        }
-      `}</style>
-    </div>
-  );
+import SideSignIn from "../components/SideSignIn";
+
+const Index = () => {
+  return <SideSignIn />;
 };
 
-export default Home;
+export default Index;
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#1e3378"
+    }
+  }
+});
+
+export default theme;
